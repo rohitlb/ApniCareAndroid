265ec1493a1adc37d2af30ac734d41fb8a6117d7
Rohit Singh <31705884+rohitlb@users.noreply.github.com>
2018-01-23 18:47:27 +0530

    -

---
+++ json structures/info_brand_comb.js
@@ -0,0 +1,45 @@
+/*
+hit /formolecule
+keys molecule , nskip ,page
+page means you want info | brands | combination
+
+
+{
+	"message": "molecule information",
+	"data": [{
+		"_id": "5a6491f346918629f8b7dc98",
+		"molecule_name": "Aceclofenac",
+		"drug_categories": "Anti-Inflammatory Agents",
+		"description": "Aceclofenac is a non-steroidal anti-inflammatory drug (NSAID) with marked anti-inflammatory and analgesic properties.\nIt is reported to have a higher anti-inflammatory action or at least comparable effects than conventional NSAIDs in double-blind studies [2, 3, 5].\nAceclofenac potently inhibits the cyclo-oxygenase enzyme (COX) that is involved in the synthesis of prostaglandins, which are inflammatory mediators that cause pain, swelling, inflammation, and fever.\nIt is orally administered for the relief of pain and inflammation in osteoarthritis, rheumatoid arthritis and ankylosing spondylitis. Aceclofenac belongs to BCS Class II as it possesses poor aqueous solubility [2].\nIt displays high permeability to penetrate into synovial joints where in patients with osteoarthritis and related conditions, the loss of articular cartilage in the area causes joint pain, tenderness, stiffness, crepitus, and local inflammation [1].\nAceclofenac is also reported to be effective in other painful conditions such as dental and gynaecological conditions [7].\nIn 1991, aceclofenac was developed as an analog of a commonly prescribed NSAID, Diclofenac, via chemical modification in effort to improve the gastrointestinal tolerability of the drug. It is a more commonly prescribed drug in Europe.",
+		"absorption": "Aceclofenac is rapidly and completely absorbed from the gastrointestinal tract and circulates mainly as unchanged drug following oral administration.\nPeak plasma concentrations are reached around 1.25 to 3 hours post-ingestion, and the drug penetrates into the synovial fluid where the concentration may reach up to 60% of that in the plasma [11].\nThere is no accumulation in regular dosing, with similar maximum plasma concentration (Cmax) and time to reach peak plasma concentration (Tmax) after single and multiple doses [2].",
+		"distribution": "The volume of distribution is approximately 25 L [11].",
+		"metabolism": "4'-hydroxyaceclofenac is the main metabolite detected in plasma however other minor metabolites include diclofenac, 5-hydroxyaceclofenac, 5-hydroxydiclofenac, and 4'-hydroxydiclofenac [2].\nIt is probable that the metabolism of aceclofenac is mediated by CYP2C9 [9]",
+		"excretion": "not available",
+		"side_effect": "",
+		"precaution": "",
+		"food": "",
+		"source": "Raza K, Kumar M, Kumar P, Malik R, Sharma G, Kaur M, Katare OP: Topical delivery of aceclofenac: challenges and promises of novel drug delivery systems. Biomed Res Int. 2014;2014:406731. doi: 10.1155/2014/406731. Epub 2014 Jun 18. [PubMed:25045671]\nLegrand E: Aceclofenac in the management of inflammatory pain. Expert Opin Pharmacother. 2004 Jun;5(6):1347-57. [PubMed:15163279]\nPareek A, Chandurkar N: Comparison of gastrointestinal safety and tolerability of aceclofenac with diclofenac: a multicenter, randomized, double-blind study in patients with knee osteoarthritis. Curr Med Res Opin. 2013 Jul;29(7):849-59. doi: 10.1185/03007995.2013.795139. Epub 2013 Apr 30. [PubMed:23581533]\nMoore RA, Derry S, McQuay HJ: Single dose oral aceclofenac for postoperative pain in adults. Cochrane Database Syst Rev. 2009 Jul 8;(3):CD007588. doi: 10.1002/14651858.CD007588.pub2. [PubMed:19588436]",
+		"__v": 0,
+		"contradictions": [{
+			"_id": "5a6491f346918629f8b7dc99",
+			"info": "",
+			"subhead": ""
+		}],
+		"dosage": [{
+			"_id": "5a6491f346918629f8b7dc9a",
+			"info": "",
+			"subhead": ""
+		}],
+		"other_interaction": [{
+			"_id": "5a6491f346918629f8b7dc9b",
+			"info": "",
+			"subhead": ""
+		}],
+		"other_drug_interaction": [{
+			"_id": "5a6491f346918629f8b7dc9c",
+			"info": "The risk or severity of adverse effects can be increased when Aceclofenac is combined with 16-Bromoepiandrosterone,The risk or severity of adverse effects can be increased when Aceclofenac is combined with 19-norandrostenedione.,The risk or severity of adverse effects can be increased when Aceclofenac is combined with 5-androstenedione.",
+			"subhead": "16-Bromoepiandrosterone,19-norandrostenedione,5-androstenedione"
+		}]
+	}]
+}
+ */
