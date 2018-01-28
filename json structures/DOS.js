8ddaaef44264eac14f335183a0ce07a7d000609d
Rohit Singh <lb.rohitsingh@gmail.com>
2018-01-28 18:13:53 +0530

    DOS

---
+++ json structures/DOS.js
@@ -0,0 +1,107 @@
+// info for disease
+/*
+hit to /dos_info
+search : Malaria
+page : disease
+{
+	"message": "read more for disease",
+	"data": [{
+		"_id": "5a648675bf575b31f8bbfd2e",
+		"disease_name": "Malaria",
+		"symptoms": "High Fever\nPain",
+		"risk_factor": "Dehydration",
+		"cause": "Mosquito Bite",
+		"treatment": "Antibiotic",
+		"outlook": "Decrease in body motility rate",
+		"prevention": "Prevent from mosquito bitep",
+		"source": "Internet",
+		"__v": 0,
+		"organs": [{
+			"_id": "5a648675bf575b31f8bbfd2f",
+			"info": "Pain in whole body,Pain with fever",
+			"subhead": "Body,Joint"
+		}],
+		"diagnosis": [{
+			"_id": "5a648675bf575b31f8bbfd30",
+			"info": "RBC count decreases",
+			"subhead": "Blood"
+		}]
+	}]
+}
+
+
+ */
+
+
+
+// info for organ
+/*
+hit to /dos_info
+search : alpha
+page : organ
+
+{
+	"message": "read more for organ",
+	"data": [{
+		"_id": "5a648f385d2a8b112c14e32c",
+		"disease_name": "alpha",
+		"symptoms": "alpha",
+		"risk_factor": "alpha",
+		"cause": "alpha",
+		"treatment": "alpha",
+		"outlook": "alpha",
+		"prevention": "alpha",
+		"source": "alpha",
+		"__v": 0,
+		"organs": [{
+			"_id": "5a648f385d2a8b112c14e32d",
+			"info": "alpha",
+			"subhead": "alpha"
+		}],
+		"diagnosis": [{
+			"_id": "5a648f385d2a8b112c14e32e",
+			"info": "alpha",
+			"subhead": "alpha"
+		}]
+	}]
+}
+
+
+ */
+
+// info for symptoms
+
+/*
+search : alpha
+page : symptom
+
+
+{
+	"message": "read more for symptom",
+	"data": [{
+		"_id": "5a648f385d2a8b112c14e32c",
+		"disease_name": "alpha",
+		"symptoms": "alpha",
+		"risk_factor": "alpha",
+		"cause": "alpha",
+		"treatment": "alpha",
+		"outlook": "alpha",
+		"prevention": "alpha",
+		"source": "alpha",
+		"__v": 0,
+		"organs": [{
+			"_id": "5a648f385d2a8b112c14e32d",
+			"info": "alpha",
+			"subhead": "alpha"
+		}],
+		"diagnosis": [{
+			"_id": "5a648f385d2a8b112c14e32e",
+			"info": "alpha",
+			"subhead": "alpha"
+		}]
+	}]
+}
+
+
+ */
+
